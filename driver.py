abe897388d31e44e20f8088fd3cedbb6c963ed72
Christina Trotter <cjntrotter@gmail.com>
2019-03-30 16:53:38 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -2,9 +2,9 @@ import os
 from variables import *
 from model import *
 
-BATCH_SIZE = 128
-EPOCHS =  10
-VERBOSE = 2
-UNITS = 500
+BATCH_SIZE = 128 #number of samples per gradient update 
+EPOCHS =  10 #number of epochs to train the model (may be less due to early stopping callback)
+VERBOSE = 2 #verbosity mode: 0 = silent, 1 = progress bar, 2 = one line per epoch
+UNITS = 500 #dimensionality of the output space
 
 make_model(BATCH_SIZE,EPOCHS,VERBOSE,UNITS)

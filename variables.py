3f21625b36b1ee0a9c9c5726cd9f24cb525f7ffb
Christina Trotter <cjntrotter@gmail.com>
2019-03-30 22:06:42 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,10 +3,10 @@ VOCAB = {} #key = word, value = number of documents word appears in
 VECTOR = [] #list of vocab words to model the bag-of-words after
 
 PATHS = {'TRAIN':'datasets/train/{}/',
-        'VALID':'datasets/valid/{}/'}
+         'VALID':'datasets/valid/{}/'}
 
 TOTALS = {'TRAIN':5659,
-        'VALID':611}
+          'VALID':611}
 
 LABELS = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I'}
 
